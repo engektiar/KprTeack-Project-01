a304a6514c8ad4e213e4d229eb53aa0a4688b14a
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2022-12-29 22:02:21 +0600

    volunteers start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Donation from "./components/Donation/Donation";
+import Volunteers from "./components/Volunteers/Volunteers";
 
 function App() {
   return <div>
     <Donation></Donation>
+    <Volunteers></Volunteers>
   </div>;
 }
 
---
+++ src/components/Volunteers/Volunteers.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Volunteers = () => {
+    return (
+        <section>
+            <div>
+                <h2>Become <span>Volunteers</span></h2>
+                <div>
+                    <hr></hr>
+                    <img src='https://onetakameal.org/uploads/ahar_settings/section_icon.png' alt=''></img>
+                    <hr></hr>
+                </div>
+                <article>
+                    Are you ready to become an international volunteer You can contribute your time, skills and knowledge through volunteering with us. This is an opportunity to create a positive impact and be a significant force for achieving peace and development. Make a difference to the lives of many!
+                </article>
+                <button>JOIN NOW</button>
+            </div>
+        </section>
+    );
+};
+
+export default Volunteers;
