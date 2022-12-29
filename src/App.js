aa671bd64de6547e14cbd2dfaec29c16ceeed473
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2022-12-29 23:29:30 +0600

    footer top some

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Donation from "./components/Donation/Donation";
+import FoodSharing from "./components/FoodSharing/FoodSharing";
 import Volunteers from "./components/Volunteers/Volunteers";
 
 function App() {
   return <div>
     <Donation></Donation>
     <Volunteers></Volunteers>
+    <FoodSharing></FoodSharing>
   </div>;
 }
 
---
+++ src/components/FoodSharing/FoodSharing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './FoodSharing.css';
+
+const FoodSharing = () => {
+    return (
+        <section className='foodsharing'>
+            <div className='container'>
+                <div className='row justify-items-center'>
+                    <h4 className='col-md-6'>HELP BY SHARING YOUR FOOD</h4>
+                    <div className='col-md-6 d-flex gap-2'>
+                        <button>SPONSOR KIDS</button>
+                        <div className='d-flex gap-2 align-items-center'>
+                            <span style={{width: '20px', height: '2px', backgroundColor: '#383634',}}></span>
+                            <i style={{fontSize: '35px', color: '#cf3720'}} class="fa-solid fa-o icon-star-half"></i>
+                            <span style={{width: '20px', height: '2px', backgroundColor: '#383634',}}></span>
+                        </div>
+                        <button>DONATE FOR FOOD</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default FoodSharing;
